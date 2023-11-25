f81b225b8739ccef32a7a6669b7496c4bd55d52c
Patrick Estrada <patrickestrada777@gmail.com>
2023-11-25 17:28:33 -0500

    Possible fix to previous commit: add commas

--- libraries/AP_InertialSensor/AP_InertialSensor.cpp
+++ libraries/AP_InertialSensor/AP_InertialSensor.cpp
@@ -779,7 +779,7 @@ const AP_Param::GroupInfo AP_InertialSensor::var_info[] = {
     // @Description: Min Gyro Z Value for Acceptance Test
     // @Units: rad/s
     // @Range -1000 1000
-    AP_GROUPINFO("_GYRO_MIN", 57, AP_InertialSensor, _gyro_min, (-1000))
+    AP_GROUPINFO("_GYRO_MIN", 57, AP_InertialSensor, _gyro_min, (-1000)),
 
     // @Param: _GYRO_MAX_X
     // @DisplayName: Maximum Gyro X Value
@@ -798,7 +798,7 @@ const AP_Param::GroupInfo AP_InertialSensor::var_info[] = {
     // @Description: Max Gyro Z Value for Acceptance Test
     // @Units: rad/s
     // @Range -1000 1000
-    AP_GROUPINFO("_GYRO_MAX", 58, AP_InertialSensor, _gyro_max, (1000))
+    AP_GROUPINFO("_GYRO_MAX", 58, AP_InertialSensor, _gyro_max, (1000)),
 
     // @Param: _ACCEL_MIN_X
     // @DisplayName: Minimum Accel X Value
@@ -817,7 +817,7 @@ const AP_Param::GroupInfo AP_InertialSensor::var_info[] = {
     // @Description: Min Accel Z Value for Acceptance Test
     // @Units: m/s/s
     // @Range -1000 1000
-    AP_GROUPINFO("_ACCEL_MIN", 59, AP_InertialSensor, _accel_min, (-1000))
+    AP_GROUPINFO("_ACCEL_MIN", 59, AP_InertialSensor, _accel_min, (-1000)),
 
     // @Param: _ACCEL_MAX_X
     // @DisplayName: Maximum Accel X Value
@@ -836,7 +836,7 @@ const AP_Param::GroupInfo AP_InertialSensor::var_info[] = {
     // @Description: Max Accel Z Value for Acceptance Test
     // @Units: m/s/s
     // @Range -1000 1000
-    AP_GROUPINFO("_ACCEL_MAX", 60, AP_InertialSensor, _accel_max, (1000))
+    AP_GROUPINFO("_ACCEL_MAX", 60, AP_InertialSensor, _accel_max, (1000)), 
 
 
     /*
